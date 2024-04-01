ab7ec6193d1bca368c3833a5ee932868882f0b9f
Shafina19 <hanumshafina@gmai.com>
2024-04-01 13:30:03 +0700

    Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Shafina</li>
+ <li>Nama Belakang : Hanum</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Ciamis Duren Jaya</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bekasi</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Berenang</li>
+ <li>Voli</li>
+ </ul>
+</section>
